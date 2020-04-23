41a3576e55140f1904f76925fb0cb78b6f5ffd12
PRoman93 <roman_prokopets@mail.ua>
2020-04-23 11:43:20 +0300

    Updates

---
+++ precache-manifest.2c31c1e1dac94396744c0844460c1a50.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7eb87045b6f3c8d49804bb9eb1a76223",
+    "url": "/tic-tac-toe/index.html"
+  },
+  {
+    "revision": "568797196a4575719bb4",
+    "url": "/tic-tac-toe/static/css/main.5baec7b9.chunk.css"
+  },
+  {
+    "revision": "df4d1dc0884481a814f8",
+    "url": "/tic-tac-toe/static/js/2.78cf0884.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/tic-tac-toe/static/js/2.78cf0884.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "568797196a4575719bb4",
+    "url": "/tic-tac-toe/static/js/main.b0bf51a5.chunk.js"
+  },
+  {
+    "revision": "28dfba9420967d301740",
+    "url": "/tic-tac-toe/static/js/runtime-main.4c12a8d1.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/tic-tac-toe/static/media/logo.5d5d9eef.svg"
+  }
+]);
